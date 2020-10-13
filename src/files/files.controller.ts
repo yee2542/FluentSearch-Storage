d28fb01667ceb8ea6f59c1ec59de200f78909ab6
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-13 16:23:08 +0700

    Add post/get file endpoint

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express'
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+    @Post('/upload')
+    async uploadFile(@Res() res: Response) {
+        return res.sendStatus(200)
+    }
+
+    @Get('/:id')
+    async getFileById(@Param('id') id: string) {
+        return id
+    }
+}
