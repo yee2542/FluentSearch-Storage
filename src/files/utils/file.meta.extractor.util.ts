191dcabc7b83056bea283f4c49e722eb0d867cd4
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-14 01:51:51 +0700

    Add utils for read filename from http chunk

---
+++ src/files/utils/file.meta.extractor.util.ts
@@ -0,0 +1,16 @@
+export type FileMetaExtractorUtilReturnType = {
+    fieldname: string,
+    filename: string,
+    contentType: string
+} | undefined
+
+export default (chunk: string): FileMetaExtractorUtilReturnType => {
+    const match = chunk.match(/Content-Disposition:.*name="(.*)";.*filename="(.*)"(?:\r\n|\n)Content-Type: (.*)/)
+    if (!match) return
+    const [, fieldname, filename, contentType] = match
+    return {
+        fieldname,
+        filename,
+        contentType
+    }
+}
