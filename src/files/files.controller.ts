8f020d147883d4edeb80fb630c30e81cb8dfe689
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-13 16:39:30 +0700

    change upload to return undeifined

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { Response } from 'express'
+import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Response } from 'express';
 @Controller('files')
 export class FilesController {
     @Post('/upload')
-    async uploadFile(@Res() res: Response) {
-        return res.sendStatus(200)
+    @HttpCode(201)
+    async uploadFile(): Promise<Response> {
+        return
     }
 
     @Get('/:id')
