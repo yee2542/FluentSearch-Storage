ea300ef64d77e11f5057adab85357f80480e69de
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-15 15:36:03 +0700

    Register multer module

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { MulterModule } from '@nestjs/platform-express'
+import { FileConfigService } from './file.config.service';
 
 @Module({
+  imports: [
+    MulterModule.registerAsync({ useClass: FileConfigService })
+  ],
   controllers: [FilesController],
   providers: [FilesService]
 })
-export class FilesModule {}
+export class FilesModule { }
