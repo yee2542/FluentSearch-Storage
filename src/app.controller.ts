a226e9dac5771f44cf4fccaca91257c5a90e2f47
yee2542 <yee2542gtl@gmail.com>
2021-05-18 02:47:08 +0700

    move uplaod to stream

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    // console.log(file);
-    // file.stream.on('data', data => console.log(data));
-  }
 
   @Get()
   getHello(): string {
---
+++ src/stream/stream.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  Post,
+  Req,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiConsumes } from '@nestjs/swagger';
+import { Request } from 'express';
+
+@Controller('stream')
+export class StreamController {
+  @ApiOperation({
+    summary: 'Upload a file.',
+    requestBody: {
+      content: {
+        'multipart/form-data': {
+          schema: {
+            type: 'object',
+            properties: { files: { type: 'string', format: 'binary' } },
+          },
+        },
+      },
+    },
+  })
+  @ApiConsumes('multipart/form-data')
+  @Post('/upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Req() req: Request,
+  ) {
+    // console.log(files[0].size);
+    // console.log(files);
+    console.log(req.files);
+  }
+}
