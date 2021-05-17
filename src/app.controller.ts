557384c15898e11ae4a5a4fe90b1df830cfcc759
yee2542 <yee2542gtl@gmail.com>
2021-05-18 00:42:27 +0700

    wip upload endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    // console.log(file);
+    // file.stream.on('data', data => console.log(data));
+  }
 
   @Get()
   getHello(): string {
