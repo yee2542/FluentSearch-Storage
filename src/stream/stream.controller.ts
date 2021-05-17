d1d6797ec173cd251585663085c8bacd7224e98a
yee2542 <yee2542gtl@gmail.com>
2021-05-18 04:57:59 +0700

    refactor

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -2,12 +2,18 @@ import {
   Controller,
   Post,
   Req,
+  Res,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { ApiOperation, ApiConsumes } from '@nestjs/swagger';
-import { Request } from 'express';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { Request, Response } from 'express';
 
 @Controller('stream')
 export class StreamController {
@@ -24,13 +30,33 @@ export class StreamController {
       },
     },
   })
+  @ApiOkResponse({
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          _id: { type: 'string', example: '5e2b447e4aadb800bccfb339' },
+          length: { type: 'number', example: 730416 },
+          chunkSize: { type: 'number', example: 261120 },
+          uploadDate: { type: 'Date', example: '2020-01-24T19:24:46.366Z' },
+          filename: { type: 'string', example: 'IMG_0359.jpeg' },
+          md5: { type: 'string', example: 'ba230f0322784443c84ffbc5b6160c30' },
+          contentType: { type: 'string', example: 'image/jpeg' },
+        },
+      },
+    },
+  })
+  @ApiBearerAuth('JWT')
   @ApiConsumes('multipart/form-data')
   @Post('/')
   @UseInterceptors(FilesInterceptor('files'))
-  uploadFile(
+  async uploadFile(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Req() req: Request,
+    @Res() res: Response,
   ) {
-    console.log(req.files);
+    const resParsed = files.map(f => ({ ...f, buffer: undefined }));
+    res.send(resParsed);
   }
 }
