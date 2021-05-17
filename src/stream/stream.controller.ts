166a85b5b7d82e83da2a342de0c8c200ef3356ec
yee2542 <yee2542gtl@gmail.com>
2021-05-18 05:40:56 +0700

    implement streamResponse

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -30,23 +30,7 @@ export class StreamController {
       },
     },
   })
-  @ApiOkResponse({
-    schema: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          _id: { type: 'string', example: '5e2b447e4aadb800bccfb339' },
-          length: { type: 'number', example: 730416 },
-          chunkSize: { type: 'number', example: 261120 },
-          uploadDate: { type: 'Date', example: '2020-01-24T19:24:46.366Z' },
-          filename: { type: 'string', example: 'IMG_0359.jpeg' },
-          md5: { type: 'string', example: 'ba230f0322784443c84ffbc5b6160c30' },
-          contentType: { type: 'string', example: 'image/jpeg' },
-        },
-      },
-    },
-  })
+  @ApiOkResponse({ type: StreamResponseDTO })
   @ApiBearerAuth('JWT')
   @ApiConsumes('multipart/form-data')
   @Post('/')
