f12ab96eb4d1f29ac993c4124d3f63bd86e9db35
yee2542 <yee2542gtl@gmail.com>
2021-05-19 05:45:16 +0700

    import jwt module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -4,10 +4,23 @@ import { ConfigModule } from '../config/config.module';
 import { MulterConfigService } from './multer.config-service';
 import { StorageService } from './storage.service';
 import { StorageController } from './storage.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '../config/config.service';
 
 @Module({
   imports: [
     ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get().jwt.secretKey,
+        signOptions: {
+          expiresIn: config.get().jwt.expires,
+          algorithm: 'HS256',
+        },
+      }),
+    }),
     MulterModule.registerAsync({
       imports: [ConfigModule],
       useClass: MulterConfigService,
