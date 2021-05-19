9d170e6b0bef1b00c2c1a17bd14c8e04f8e1cc83
yee2542 <yee2542gtl@gmail.com>
2021-05-20 04:07:41 +0700

    add file schema

---
+++ src/storage/schemas/file.schema.ts
@@ -0,0 +1,21 @@
+import { BaseFileSchema, FileTypeEnum } from 'fluentsearch-types';
+import { Schema } from 'mongoose';
+
+const fileSchema = new Schema<FileDocument>({
+  meta: { type: Object },
+  owner: { type: String, index: true },
+  zone: { type: String, index: true },
+  label: { type: String },
+
+  createDate: { type: Date, default: Date.now, index: true },
+  updateDate: { type: Date, default: Date.now, index: true },
+});
+
+export default fileSchema;
+
+export interface FileDocument
+  extends Document,
+    BaseFileSchema<
+      FileTypeEnum.Image | FileTypeEnum.ImageThumbnail | FileTypeEnum.Video,
+      any
+    > {}
