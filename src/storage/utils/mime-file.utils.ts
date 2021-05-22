3402eaae7716155a77c1d077bcdf1ae21b890d06
yee2542 <yee2542gtl@gmail.com>
2021-05-22 21:25:25 +0700

    refactor

--- src/storage/utils/mime-file.utils.ts
+++ src/storage/utils/mime-file.utils.ts
@@ -1,7 +1,7 @@
 import { FileTypeEnum } from 'fluentsearch-types';
 import { InvalidMimeFileExceptiomn } from '../exceptions/invalid-mime-file.exception';
 
-export default (mime: string) => {
+export default (mime: string): FileTypeEnum => {
   if (mime.includes('image')) return FileTypeEnum.Image;
   if (mime.includes('video')) return FileTypeEnum.Video;
   throw new InvalidMimeFileExceptiomn();
