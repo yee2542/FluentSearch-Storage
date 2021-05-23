bbabc09a8d1f1fa18c08ca10750acddffefa8cf9
yee2542 <yee2542gtl@gmail.com>
2021-05-24 05:31:49 +0700

    fix bad mapp file type

--- src/common/thumbnailObjectFilename.util.ts
+++ src/common/thumbnailObjectFilename.util.ts
@@ -2,6 +2,7 @@ import { FileDocument, FileTypeEnum } from 'fluentsearch-types';
 import { LeanDocument } from 'mongoose';
 
 export const IMAGE_THUMBNAIL_EXTENSION = 'jpeg';
+export const VIDEO_THUMBNAIL_EXTENSION = 'gif';
 
 export default (
   thumbnailMeta: FileDocument | LeanDocument<FileDocument>,
@@ -9,4 +10,8 @@ export default (
 ) =>
   `${thumbnailMeta._id.toHexString()}-${
     thumbnailMeta.original_filename
-  }-${type}.${IMAGE_THUMBNAIL_EXTENSION}`;
+  }-${type}.${
+    thumbnailMeta.type === FileTypeEnum.ImageThumbnail
+      ? IMAGE_THUMBNAIL_EXTENSION
+      : VIDEO_THUMBNAIL_EXTENSION
+  }`;
