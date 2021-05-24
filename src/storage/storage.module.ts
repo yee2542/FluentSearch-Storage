9d66da1c54eb40e5a5fb918ff841a8b0c9636b99
yee2542 <yee2542gtl@gmail.com>
2021-05-24 23:05:32 +0700

    remove client rabbitmq

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
-import { ConfigModule } from '../config/config.module';
-import { MulterConfigService } from './multer.config-service';
-import { StorageService } from './storage.service';
-import { StorageController } from './storage.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '../config/config.service';
-import { MinioModule } from 'nestjs-minio-client';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 import { FILES_SCHEMA_NAME } from 'fluentsearch-types';
 import fileSchema from 'fluentsearch-types/dist/entity/file.entity';
+import { MinioModule } from 'nestjs-minio-client';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { MulterConfigService } from './multer.config-service';
+import { StorageController } from './storage.controller';
+import { StorageService } from './storage.service';
 
 const MinioInstance = MinioModule.registerAsync({
   imports: [ConfigModule],
